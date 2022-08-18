a54ee90ce3194228138562be30080fa4bb0cffd3
joaovitorsb97 <jvs.bastos@gmail.com>
2022-08-17 22:41:10 -0300

    CarDTO, ParkingSpotDTO

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,27 @@
+package com.api.parkingcontrol.dtos;
+
+import javax.validation.constraints.NotBlank;
+
+public class ParkingSpotDto {
+
+    @NotBlank
+    private String parkingSpotNumber;
+    @NotBlank
+    private String block;
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+}
